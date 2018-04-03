d7cfc58678c23fcbe1531d4fe9f8c48fc7a2d4a6
Subramanyam Shastri <shastri9999@gmail.com>
2018-04-04 02:21:44 +0530

    Adding HTML Webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.tsx',
   output: {
@@ -18,4 +20,10 @@ module.exports = {
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo Manager',
+      template: 'index.html',
+    }),
+  ],
 };
