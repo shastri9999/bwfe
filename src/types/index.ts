e972fbf032ed2443bb859007506fb1bf0281110f
Subramanyam Shastri <shastri9999@gmail.com>
2018-04-04 06:08:24 +0530

    Adding Todo types

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export enum UserType {
+  Employee = 'Employee',
+  Manager = 'Manager',
+}
+
+export enum TodoStatus {
+  Completed = 'Completed',
+  Incomplete = 'Incomplete',
+}
+
+export interface ITodo {
+  id: string;
+  title: string;
+  summary: string;
+  status: TodoStatus;
+}
+
+export interface IStoreState {
+  readonly authenticatedUser: UserType | null;
+  readonly todos: ReadonlyArray<ITodo>;
+}
