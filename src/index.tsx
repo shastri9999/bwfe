0354b7ef306b72ba78bba6502873a0f2607c574c
Subramanyam Shastri <shastri9999@gmail.com>
2018-04-04 19:15:32 +0530

    Adding store and provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+import rootReducer from './reducers';
 import Heading from './components/Heading';
+import { IStoreState } from './types';
 import './styles/app.scss';
 
-ReactDOM.render(
-  <Heading title="Hello World" />,
-  document.getElementById('app')
+const store = createStore<IStoreState>(rootReducer);
+
+const app = (
+  <Provider store={store}>
+    <Heading title="Hello World" />
+  </Provider>
 );
+
+ReactDOM.render(app, document.getElementById('app'));
