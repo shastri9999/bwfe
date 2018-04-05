bfc18cbfda220a6733b11e76fa6b0b961ac7e3fc
Subramanyam Shastri <shastri9999@gmail.com>
2018-04-05 18:39:50 +0530

    Adding logic for showing All filter on login

--- src/components/LoginPage/index.tsx
+++ src/components/LoginPage/index.tsx
@@ -3,13 +3,17 @@ import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { bindActionCreators, Dispatch } from 'redux';
 
-import { signIn as signInAction } from '../../actions';
-import { ISignInAction } from '../../actions/ActionTypes';
+import {
+  changeFilter as changeFilterAction,
+  signIn as signInAction,
+} from '../../actions';
+import { IChangeFilterAction, ISignInAction } from '../../actions/ActionTypes';
 import { todosPath } from '../../routes/paths';
 import { IStoreState, UserType } from '../../types';
 import Button from '../Button';
 
 export interface ILoginPageProps extends RouteComponentProps<any> {
+  changeFilter: (visibilityFilter: null) => IChangeFilterAction;
   signIn: (role: UserType) => ISignInAction;
 }
 
@@ -29,6 +33,7 @@ class LoginPage extends React.Component<ILoginPageProps> {
   }
 
   private onLogin = (role: UserType) => {
+    this.props.changeFilter(null);
     this.props.history.push(todosPath);
     this.props.signIn(role);
   };
@@ -43,6 +48,7 @@ class LoginPage extends React.Component<ILoginPageProps> {
 }
 
 const mapDispatchToProps = (dispatch: Dispatch<IStoreState>) => ({
+  changeFilter: bindActionCreators(changeFilterAction, dispatch),
   signIn: bindActionCreators(signInAction, dispatch),
 });
 
